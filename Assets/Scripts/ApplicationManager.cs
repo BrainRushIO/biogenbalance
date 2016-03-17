b09d68f6b2667d8d942659d176df34b979ce7f8e
Eric Espinoza <ericespinoza@outlook.com>
2016-03-17 16:02:10 -0700

    Introduced new scenes. Started setting up UI manager and Application Manager.

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ApplicationManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+	
+	}
+
+	void PopulateSceneDictionary() {
+		
+	}
+
+	/// <summary>
+	/// Loads the selected scene when one of the top UI buttons is pressed.
+	/// </summary>
+	/// <param name="sceneInitials">The initials that correspond to the specific scene.</param>
+	public void LoadScene( string sceneInitials ) {
+
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	public static UIManager s_instance;
+
+	[Header("Top Bar")]
+	public Button familiarizeButton,
+				acquireButton,
+				practiceButton,
+				validateButton,
+				progressButton;
+	public RectTransform familiarizeDropdownItems,
+				acquireDropdownItems,
+				practiceDropdownItems;
+
+	[Header("Side Bar")]
+	public RectTransform sidepanel;
+	public Text listViewText,
+				descriptionViewText;
+
+	[Header("Tools")]
+	public Button pointerToolButton,
+				rotateToolButton,
+				panToolButton,
+				forcepsToolButton;
+
+	void Awake() {
+		if( s_instance == null ) {
+			s_instance = this;
+			DontDestroyOnLoad( gameObject );
+		} else {
+			DestroyImmediate( gameObject );
+		}
+	}
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
