c1e0c35a713bed474407609f8742b8945a120d10
Eric Espinoza <ericespinoza@outlook.com>
2016-03-21 16:50:50 -0700

    Fixed bug where traveling to other scenes wasn't working.

--- Assets/Scripts/MenuBarButton.cs
+++ Assets/Scripts/MenuBarButton.cs
@@ -7,24 +7,35 @@ public class MenuBarButton : MonoBehaviour {
 	[System.NonSerialized]
 	public bool isSelected;
 
+	private bool isHoveringOverDropdown = false;
+
 	public void WasClicked() {
-		if (isSelected) {
-			if (dropDownMenuObject != null)
-				dropDownMenuObject.SetActive (false);
-		} else {
+		if (!isSelected) {
 			UIManager.s_instance.DeselectTopBarButtons ();
 			UIManager.s_instance.CloseDropDowns ();
 
 			if (dropDownMenuObject != null)
 				dropDownMenuObject.SetActive (true);
+		
+		} else  {
+			if (dropDownMenuObject != null)
+				dropDownMenuObject.SetActive (false);
 		}
 		isSelected = !isSelected;
 	}
 
 	public void Deselect() {
 		isSelected = false;
-		if( dropDownMenuObject != null )
+		if( dropDownMenuObject != null && !isHoveringOverDropdown )
 			dropDownMenuObject.SetActive (false);
 		//TODO button color stuff
+	}
+
+	/// <summary>
+	/// Sets the is hovering over dropdown bool. This is called from the Event Trigger on the Dropdown objects in the scene.
+	/// </summary>
+	/// <param name="val">If set to <c>true</c> value.</param>
+	public void SetIsHoveringOverDropdown( bool val ) {
+		isHoveringOverDropdown = val;
 	}
 }
