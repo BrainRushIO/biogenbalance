5b30c75bafd51c151e9b3414e598720be49557c0
Eric Espinoza <ericespinoza@outlook.com>
2016-03-21 15:13:52 -0700

    Updates to dropdown and menu buttons.

---
+++ Assets/Scripts/MenuBarButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuBarButton : MonoBehaviour {
+	public GameObject dropDownMenuObject;
+	[System.NonSerialized]
+	public bool isSelected;
+
+	public void WasClicked() {
+		if (isSelected) {
+			if (dropDownMenuObject != null)
+				dropDownMenuObject.SetActive (false);
+		} else {
+			UIManager.s_instance.DeselectTopBarButtons ();
+			UIManager.s_instance.CloseDropDowns ();
+
+			if (dropDownMenuObject != null)
+				dropDownMenuObject.SetActive (true);
+		}
+		isSelected = !isSelected;
+	}
+
+	public void Deselect() {
+		isSelected = false;
+		if( dropDownMenuObject != null )
+			dropDownMenuObject.SetActive (false);
+		//TODO button color stuff
+	}
+}
