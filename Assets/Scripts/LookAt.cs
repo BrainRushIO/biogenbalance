7a68a7666611de7173eb9ee9d5d8e69606ce57d5
Eric Espinoza <ericespinoza@outlook.com>
2016-03-25 01:02:10 -0700

    Serial Port content in microbalance.

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -9,6 +9,12 @@ public class LookAt : MonoBehaviour {
 	/// </summary>
 	public Transform cam;
 
+	void Start() {
+		if( cam == null && ApplicationManager.s_instance.currentApplicationMode == ApplicationManager.ApplicationMode.Familiarize ) {
+			cam = FamiliarizeManager.s_instance.sceneCamera.transform;
+		}
+	}
+
 	void Update () {
 		//transform.RotateAround (target.position, Vector3.up, Input.GetAxis ("Mouse X") * dragSpeed);
 		transform.LookAt(transform.position + cam.rotation * Vector3.forward, cam.rotation * Vector3.up);
