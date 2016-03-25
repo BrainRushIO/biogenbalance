cf6f339ae572623ee5214bf3291fe02cd1d1ac5e
Eric Espinoza <ericespinoza@outlook.com>
2016-03-24 17:35:02 -0700

    Work on popup bubbles.

---
+++ Assets/Scripts/ConnectLineRenderer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class ConnectLineRenderer : MonoBehaviour {
+
+	public Transform endPoint;
+
+	private LineRenderer myLineRenderer;
+
+	void Start () {
+		myLineRenderer = GetComponent<LineRenderer>();
+		Vector3[] rendererPositions = new Vector3[2] {transform.position, endPoint.position};
+		myLineRenderer.SetPositions( rendererPositions );
+	}
+}
