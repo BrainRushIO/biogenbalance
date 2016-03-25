1732dba7cccf2439f6eba6c6002b22782f946a19
Eric Espinoza <ericespinoza@outlook.com>
2016-03-24 21:56:07 -0700

    Added readout display content to microbalance.

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -4,22 +4,11 @@ using UnityEngine.UI;
 
 public class LookAt : MonoBehaviour {
 
+	/// <summary>
+	/// The in-game/scene camera. Not the UI camera.
+	/// </summary>
 	public Transform cam;
 
-
-
-	// Use this for initialization
-	void Awake(){
-		//transform.LookAt (cam);
-	}
-
-	void Start () {
-
-		//transform.LookAt (cam);
-
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		//transform.RotateAround (target.position, Vector3.up, Input.GetAxis ("Mouse X") * dragSpeed);
 		transform.LookAt(transform.position + cam.rotation * Vector3.forward, cam.rotation * Vector3.up);
