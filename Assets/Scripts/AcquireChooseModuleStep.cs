7e6143d88dd42745c452baa7714a926ade458a91
Eric Espinoza <ericespinoza@outlook.com>
2016-04-12 14:32:43 -0700

    Moved some objects in a couple acquire scenes. More work on getting Acquire to work.

---
+++ Assets/Scripts/AcquireChooseModuleStep.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AcquireChooseModuleStep : BaseAcquireModuleStep {
+
+	protected override void Start () {
+	
+	}
+
+	public override AcquireModuleStepInitializationData GetStepInitData() {
+		return new AcquireModuleStepInitializationData();
+	}
+
+	public override void ExecuteStepLogic( int index ) {
+		
+	}
+}
--- Assets/Scripts/BaseAcquireModuleStep.cs
+++ Assets/Scripts/BaseAcquireModuleStep.cs
@@ -2,12 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public struct AcquireModuleStepInitializationData {
+	public Dictionary<string, bool> objectToggles;
+	public Dictionary<string, string> messagesToParent;
+}
+
 /// <summary>
 /// The class that include the input and setup data for a single step in the Acquire module.
 /// </summary>
 public abstract class BaseAcquireModuleStep : MonoBehaviour {
 
 	public Transform cameraPosition, cameraPivot;
+	public AcquireModuleStepInitializationData initData;
 
-	public abstract void InitializeScene();
+	protected virtual void Start() {
+		initData = new AcquireModuleStepInitializationData();
+	}
+
+	public abstract AcquireModuleStepInitializationData GetStepInitData();
+
+	public abstract void ExecuteStepLogic( int index );
 }
