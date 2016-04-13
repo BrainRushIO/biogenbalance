0d9fb933da1c07ad832bb8bc05c64dace1d7b2db
Eric Espinoza <ericespinoza@outlook.com>
2016-04-12 20:50:30 -0700

    Snapping to camera positions on A4 works.

---
+++ Assets/Scripts/AcquireUseBalanceManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AcquireUseBalanceManager : BaseAcquireSubmodule {
+
+	protected override void Init() {
+		base.Init();
+	}
+
+	public override void UpdateSceneContents( int stepIndex ) {
+	}
+
+	public override void ResetScene() {
+	}
+}
---
+++ Assets/Scripts/AcquireUseModuleStep.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AcquireUseModuleStep : BaseAcquireModuleStep {
+
+	protected override void Start () {
+		base.Start();
+	}
+
+	/// <summary>
+	/// Executes the step logic. This is called from the Submodule Manager. Any logic that can't be expressed via simple bool toggles goes here. The index is the sibling index of this object.
+	/// </summary>
+	public override void ExecuteStepLogic() {
+		int index = transform.GetSiblingIndex();
+
+		switch( index )
+		{
+		default:
+			Debug.LogError( "Cannot execute step logic for index "+ index +". Index out of range." );
+			break;
+		}
+	}
+}
